7595ab513be7315c92df091f968bc296f3164dd3
enriquevel <enriquev@ciencias.unam.mx>
2025-09-18 14:33:03 -0700

    Constructores

--- src/myp/practica3/ninjas/VolunteerNinja.java
+++ src/myp/practica3/ninjas/VolunteerNinja.java
@@ -1,7 +1,12 @@
 public class VolunteerNinja extends Ninja {
     protected Rank rank;
 
-    VolunteerNinja() {
-        
+    VolunteerNinja(String name, short age, String clan, short skillLevel, Rank rank) {
+        super(name, age, clan, skillLevel);
+        this.rank = rank;
+    }
+
+    public Rank getRank() {
+        return this.rank;
     }
 }

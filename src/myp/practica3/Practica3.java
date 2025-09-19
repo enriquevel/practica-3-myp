1edb0234ac74a0a369358d49df492b06b2af590f
Luis Adrian Galindo Ruvalcaba <89614696+luis-galindo@users.noreply.github.com>
2025-09-18 20:11:25 -0600

    Implementacion inicial del patron Builder para paquetes ninja (#5)
    * Implementacion inicial del patron Builder para paquetes ninja
    
    * Mejoras en el director del patron
    
    Resta revisar la logica de los paquetes personalizados, pero tambien se requiere de otra clase que maneje la interaccion con el usuario.

--- src/myp/practica3/Practica3.java
+++ src/myp/practica3/Practica3.java
@@ -1,4 +1,7 @@
 package myp.practica3;
 
 public class Practica3 {
+	public static void main(String[] args) {
+
+	}
 }
--- src/myp/practica3/builder/PackageDirector.java
+++ src/myp/practica3/builder/PackageDirector.java
@@ -1,4 +1,28 @@
 package myp.practica3.builder;
 
 public class PackageDirector {
+
+	public Package getBasicPackage() {
+		return new DefaultPackageBuilder().addKunai(1)
+				.addShuriken(1)
+				.addAidKit(1)
+				.build();
+	}
+
+	public Package getAdvancedPackage() {
+		return new DefaultPackageBuilder().addShuriken(2)
+				.addPaperBomb(3)
+				.addSmokeBomb(2)
+				.addAidKit(2)
+				.build();
+	}
+
+	public Package getTacticalPackage() {
+		return new DefaultPackageBuilder().addKunai(3)
+				.addShuriken(2)
+				.addPaperBomb(4)
+				.addSmokeBomb(2)
+				.build();
+	}
+
 }
